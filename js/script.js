ab7c391fef7c2f96d9ee078ea02a6c5772ed8fa6
mra33 <miekob@gmail.com>
2020-05-19 16:38:21 -0700

    Adding files to test repo

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+      $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows:true,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+});
+ 
